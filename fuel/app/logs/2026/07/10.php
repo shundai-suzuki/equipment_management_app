92be822fa1edb97645ebedc1d64f76ed1b8a3a87
shundai-suzuki <shundai.suzuki@gmail.com>
2026-07-31 00:27:52 +0900

    initial commit

---
+++ fuel/app/logs/2026/07/10.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2026-07-10 01:39:50 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2026-07-10 01:55:54 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
